e60a04d229dd364923cf5a35bb63c6d586a854fa
Kornrade <kornrade@gmail.com>
2015-02-09 23:53:16 +0200

    Contact.php
    updating logic

--- Helpertools/contact.php
+++ Helpertools/contact.php
@@ -4,18 +4,33 @@ $mesaj = $_POST['mesaj'];
    
    if($_POST['trimite'])
    {
-	  if ((strlen($email)>=5)and(strlen($mesaj)>=1))	
-	      {
-		   $header = "From:<" . $email . ">";
-		   $destinatar = "chichitza@chichitza.ro";
-		   $titlu = 'Mesaj chichitza';
+       $defaultemail = "Anonymous";
+       $defaultmesaj = "No message...";
+       
+	   if ((strlen($email)<1))	
+       {
+         $email = $defaultemail;
+       }
+        
+       if ((strlen($mesaj)<1))	
+       {
+         $mesaj = $defaultmesaj;
+       }
+       
+       if( ($email != $defaultemail) || ($mesaj != $defaultmesaj) )
+       {
+           $header = "From:<" . $email . ">";
+           $destinatar = "chichitza@chichitza.ro";
+           $titlu = 'Mesaj chichitza';
            mail($destinatar, $titlu, $mesaj, $header);
-		   ?>
-		   <script language=JavaScript>
-           window.close();
-           </script>
-		   <?
-		  }
+       }
+       
+       ?>
+       <script language=JavaScript>
+       window.close();
+       </script>
+       <?
+
    }
     
 ?>
